51e1d052b7ceeb8cc61ab80bb78995d4a4866304
Yosuke Yamakawa <tech@y-yamakawa.com>
2022-02-18 18:44:02 +0900

    リスト2-3を追加

---
+++ chapter2/list2-3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"module_hello/hello"
+	"strconv"
+)
+
+func main() {
+	x := hello.Input("type a price")
+	n, err := strconv.Atoi(x)
+	if err != nil {
+		fmt.Println("ERROR!!")
+		return
+	}
+	p := float64(n)
+	fmt.Println(int(p * 1.1))
+}
