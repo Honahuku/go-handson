e6cda4a70c99b00c3dab0a327cf78d7ea1cf7b00
Yosuke Yamakawa <tech@y-yamakawa.com>
2022-02-19 08:41:36 +0900

    ディレクトリ調整、2-9追加

---
+++ chapter2/2-2/list2-2.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// x := hello.Input("type a price")
+	// n, err := strconv.Atoi(x) //コンパイルエラー
+	// p := float64(n)
+	// fmt.Println(int(p * 1.1))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module module_hello
 
-go 1.15
+go 1.17
